db4aba8a3dd113df9916149a92aeab80293df969
Paul Moore <pdmoore@gmail.com>
2017-10-28 20:55:41 -0700

    add test for already increasing sequence

--- java/almostIncreasingSequence/src/com/pdm/codefights/Main.java
+++ java/almostIncreasingSequence/src/com/pdm/codefights/Main.java
@@ -2,6 +2,8 @@ package com.pdm.codefights;
 
 public class Main {
 
+    // Rename class and remove the Main method
+
     public static void main(String[] args) {
 	// write your code here
     }
--- java/almostIncreasingSequence/test/almostIncreasingSequenceTest.java
+++ java/almostIncreasingSequence/test/almostIncreasingSequenceTest.java
@@ -4,9 +4,25 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class almostIncreasingSequenceTest {
 
+    // Need to see that more than one test works
+
     @Test
     public void sequenceOfTwoElementsIsAlwaysIncreasing() {
         int[] sequence = new int[] { 1, 1 };
+        boolean actual = Main.almostIncreasingSequence(sequence);
+        assertEquals(true, actual);
+    }
+
+    @Test
+    public void sequenceOfIncreasingElementsIsIncreasing() {
+        int[] sequence = new int[] { 1, 2, 3 };
+        boolean actual = Main.almostIncreasingSequence(sequence);
+        assertEquals(true, actual);
+    }
+
+    @Test
+    public void sequenceWithJustOneAnamolyIsIncreasing() {
+        int[] sequence = new int[] { 1, 2, 99, 3 };
         boolean actual = Main.almostIncreasingSequence(sequence);
         assertEquals(true, actual);
     }
