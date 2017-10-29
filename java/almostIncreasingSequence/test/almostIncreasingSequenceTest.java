4af93e085d576ad7ddaa12ede7dc65f2ce108262
Paul Moore <pdmoore@gmail.com>
2017-10-28 21:08:22 -0700

    add tests that cover existing logic

--- java/almostIncreasingSequence/test/almostIncreasingSequenceTest.java
+++ java/almostIncreasingSequence/test/almostIncreasingSequenceTest.java
@@ -18,12 +18,28 @@ public class almostIncreasingSequenceTest {
         int[] sequence = new int[] { 1, 2, 3 };
         boolean actual = Main.almostIncreasingSequence(sequence);
         assertEquals(true, actual);
+
+        sequence = new int[] { 123, -17, -5, 1, 2, 3, 12, 43, 45 }; // Test 16
+        actual = Main.almostIncreasingSequence(sequence);
+        assertEquals(true, actual);
     }
 
     @Test
     public void sequenceWithJustOneAnomalyIsIncreasing() {
         int[] sequence = new int[] { 1, 3, 2 }; // Test 2
         boolean actual = Main.almostIncreasingSequence(sequence);
+        assertEquals(true, actual);
+
+        sequence = new int[] { 1, 2, 3, 4, 3, 6 };  // Test 14
+        actual = Main.almostIncreasingSequence(sequence);
+        assertEquals(true, actual);
+
+        sequence = new int[] { 1, 2, 3, 4, 99, 5, 6 };  // Test 15
+        actual = Main.almostIncreasingSequence(sequence);
+        assertEquals(true, actual);
+
+        sequence = new int[] { 3, 5, 67, 98, 3 };  // Test 17
+        actual = Main.almostIncreasingSequence(sequence);
         assertEquals(true, actual);
     }
 
