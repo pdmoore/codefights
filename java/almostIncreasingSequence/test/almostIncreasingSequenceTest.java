542da8a5f96635a697b5b6c23ec1cbe271700c23
Paul Moore <pdmoore@gmail.com>
2017-10-30 18:41:08 -0700

    organize imports

--- java/almostIncreasingSequence/test/almostIncreasingSequenceTest.java
+++ java/almostIncreasingSequence/test/almostIncreasingSequenceTest.java
@@ -3,7 +3,6 @@ import org.junit.jupiter.api.Test;
 
 import java.time.Duration;
 
-import static java.time.Duration.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTimeout;
 
