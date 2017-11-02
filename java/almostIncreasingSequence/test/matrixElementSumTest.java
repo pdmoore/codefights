e5505f86ab78ed66edd3024a169a04185dd36f72
Paul Moore <pdmoore@gmail.com>
2017-11-02 16:04:49 -0700

    another test for haunted rooms

--- java/almostIncreasingSequence/test/matrixElementSumTest.java
+++ java/almostIncreasingSequence/test/matrixElementSumTest.java
@@ -35,5 +35,15 @@ public class matrixElementSumTest {
 
         int actual = Main.matrixElementSum(matrix);
         assertEquals(9, actual);
+
+        matrix = new int[][] {      // test 2
+                {1, 1, 1,  0},
+                {0, 5, 0,  1},
+                {2, 1, 3, 10}
+        };
+
+        actual = Main.matrixElementSum(matrix);
+        assertEquals(9, actual);
+
     }
 }
