b07ea37f68aeb1e29ab4a4b096d99a41b57a43da
Paul Moore <pdmoore@gmail.com>
2018-01-21 21:36:58 -0800

    simple impl handles base cases

--- java/almostIncreasingSequence/test/arcade/exploringthewaters/AlternatingSumsTest.java
+++ java/almostIncreasingSequence/test/arcade/exploringthewaters/AlternatingSumsTest.java
@@ -13,7 +13,28 @@ public class AlternatingSumsTest {
         assertArrayEquals(expected, alternatingSums(input));
     }
 
+    @Test
+    void onlyOneElement_SecondSumIsZero() {
+        int[] input = new int[] { 80 };
+        int[] expected = new int[] {80, 0};
+        assertArrayEquals(expected, alternatingSums(input));
+    }
+
     private int[] alternatingSums(int[] input) {
-        return input;
+        int sum1 = 0;
+        int sum2 = 0;
+
+        for (int i = 0; i < input.length; i++) {
+            if (i % 2 == 0) {
+                sum1 += input[i];
+            } else {
+                sum2 += input[i];
+            }
+        }
+
+        int[] result = new int[2];
+        result[0] = sum1;
+        result[1] = sum2;
+        return result;
     }
 }
