8bbb693b3060602a297684625a3027b106d4f9de
Paul Moore <pdmoore@gmail.com>
2018-02-20 12:49:11 -0800

    Starting to refactor to solution.

--- java/almostIncreasingSequence/test/arcade/exploringthewaters/AddBorderTest.java
+++ java/almostIncreasingSequence/test/arcade/exploringthewaters/AddBorderTest.java
@@ -2,6 +2,9 @@ package arcade.exploringthewaters;
 
 import org.junit.jupiter.api.Test;
 
+
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 public class AddBorderTest {
@@ -17,9 +20,15 @@ public class AddBorderTest {
 
     private String[] addBorder(String[] input) {
         String[] result = new String[input.length + 2];
-        result[0] = "***";
+        result[0] = addOuterBorder(input[0].length());
         result[1] = "*" + input[0] + "*";
         result[2] = "***";
         return result;
+    }
+
+    private String addOuterBorder(int length) {
+        char[] result = new char[length + 2];
+        Arrays.fill(result, '*');
+        return new String(result);
     }
 }
