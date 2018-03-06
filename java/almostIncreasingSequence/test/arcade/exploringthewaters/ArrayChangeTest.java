ce1083642c5e39f33550077b13d088bb563ee3ec
Paul Moore <pdmoore@gmail.com>
2018-03-05 20:42:33 -0800

    Case where one element needs to increase by 1 - naive impl.

--- java/almostIncreasingSequence/test/arcade/exploringthewaters/ArrayChangeTest.java
+++ java/almostIncreasingSequence/test/arcade/exploringthewaters/ArrayChangeTest.java
@@ -3,16 +3,30 @@ package arcade.exploringthewaters;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class ArrayChangeTest {
 
     @Test
     public void SequenceIsAlreadyIncreasing_ZeroMoves() {
         int[] inputArray = new int[] { 1, 2, 3 };
-        Assertions.assertEquals(0, arrayChange(inputArray));
+        assertEquals(0, arrayChange(inputArray));
+    }
 
+    @Test
+    public void SequenceOneElementNeedsToIncreaseByOne_OneMove() {
+        int[] inputArray = new int[] { 1, 1 };
+        assertEquals(1, arrayChange(inputArray));
     }
 
     private int arrayChange(int[] inputArray) {
-        return 0;
+        int moves = 0;
+        int previousValue = inputArray[0];
+
+        if (inputArray[1] <= inputArray[0]) {
+            moves += inputArray[1] - inputArray[0] + 1;
+        }
+
+        return moves;
     }
 }
