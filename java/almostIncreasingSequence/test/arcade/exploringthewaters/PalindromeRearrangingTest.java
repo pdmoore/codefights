4c9dfd34bb81f7d37f88d48210709994eaa2ad9d
Paul Moore <pdmoore@gmail.com>
2018-03-08 20:35:35 -0600

    Negative path test - way too big a step on the impl.

--- java/almostIncreasingSequence/test/arcade/exploringthewaters/PalindromeRearrangingTest.java
+++ java/almostIncreasingSequence/test/arcade/exploringthewaters/PalindromeRearrangingTest.java
@@ -2,6 +2,10 @@ package arcade.exploringthewaters;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class PalindromeRearrangingTest {
@@ -18,7 +22,33 @@ public class PalindromeRearrangingTest {
         assertTrue(palindromeRearranging(input));
     }
 
+    @Test
+    public void FourLetterStringWithNoSameLetters_IsNotAPalindrome() {
+        String input = "abca";
+        assertFalse(palindromeRearranging(input));
+    }
+
     private boolean palindromeRearranging(String inputString) {
-        return true;
+        if (inputString.length() == 1) return true;
+
+        Map<Character, Integer> letterCount = new HashMap<>();
+        for (int i = 0; i < inputString.length(); i++) {
+            if (letterCount.containsKey(inputString.charAt(i))) {
+                int countOfThisLetter = letterCount.get(inputString.charAt(i));
+                countOfThisLetter++;
+                letterCount.put(inputString.charAt(i), countOfThisLetter);
+            } else {
+                letterCount.put(inputString.charAt(i), 1);
+            }
+        }
+
+        int numbereOfSingleCharacters = 0;
+        for (Character letter :
+                letterCount.keySet()) {
+            if (letterCount.get(letter) == 1) {
+                numbereOfSingleCharacters++;
+            }
+        }
+        return (numbereOfSingleCharacters <= 1);
     }
 }
