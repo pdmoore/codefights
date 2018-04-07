e978be4f31cf4a08eefc194036ee0cb55f2644f4
Paul Moore <pdmoore@gmail.com>
2018-04-07 16:29:37 -0700

    add a positive test and a naive impl

--- java/almostIncreasingSequence/test/arcade/islandofknowledge/AreEquallyStrongTest.java
+++ java/almostIncreasingSequence/test/arcade/islandofknowledge/AreEquallyStrongTest.java
@@ -3,15 +3,23 @@ package arcade.islandofknowledge;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class AreEquallyStrongTest {
+
+    @Test
+    public void BothArmsAreSame() {
+        assertTrue(areEquallyStrong(10, 15, 10, 15));
+    }
 
     @Test
     public void StrongArmsMatch_WeakArmsDoNot() {
         assertFalse(areEquallyStrong(15, 10, 15, 9));
     }
 
+
+
     boolean areEquallyStrong(int yourLeft, int yourRight, int friendsLeft, int friendsRight) {
-        return false;
+        return yourLeft + yourRight == friendsLeft + friendsRight;
     }
 }
