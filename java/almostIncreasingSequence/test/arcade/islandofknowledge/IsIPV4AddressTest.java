efdfb1664e36016e6d2375712c1a17a2c40e414a
Paul Moore <pdmoore@gmail.com>
2018-04-21 17:16:41 -0700

    Invalid when any element is not a number (cannot be parsed as an Integer)

--- java/almostIncreasingSequence/test/arcade/islandofknowledge/IsIPV4AddressTest.java
+++ java/almostIncreasingSequence/test/arcade/islandofknowledge/IsIPV4AddressTest.java
@@ -22,11 +22,20 @@ public class IsIPV4AddressTest {
         assertFalse(isIPv4Address(".254.255.0"), "Should have 4 elements");
     }
 
+    @Test
+    public void InvalidAddress_ElementNaN() {
+        assertFalse(isIPv4Address("1.1.1.1a"), "Elements should all be Integers");
+    }
+
     private boolean isIPv4Address(String inputString) {
         String[] elements = inputString.split("\\.");
         for (String element : elements) {
             if (element.isEmpty()) return false;
-            if (Integer.parseInt(element) > 255) return false;
+            try {
+                if (Integer.parseInt(element) > 255) return false;
+            } catch (NumberFormatException e) {
+                return false;
+            }
         }
         return true;
     }
