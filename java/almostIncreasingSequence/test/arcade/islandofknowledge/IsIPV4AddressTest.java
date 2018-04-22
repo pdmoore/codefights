7b79ebefad2c8e0b995fa078775f4d3dfc5fa91e
Paul Moore <pdmoore@gmail.com>
2018-04-21 17:12:26 -0700

    Invalid when any element greater than 255

--- java/almostIncreasingSequence/test/arcade/islandofknowledge/IsIPV4AddressTest.java
+++ java/almostIncreasingSequence/test/arcade/islandofknowledge/IsIPV4AddressTest.java
@@ -2,6 +2,7 @@ package arcade.islandofknowledge;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class IsIPV4AddressTest {
@@ -11,7 +12,16 @@ public class IsIPV4AddressTest {
         assertTrue(isIPv4Address("172.16.254.1"));
     }
 
+    @Test
+    public void InvalidAddress_ElementExceedsMaximum() {
+        assertFalse(isIPv4Address("172.316.254.1"), "Elements should be in range 0..255");
+    }
+
     private boolean isIPv4Address(String inputString) {
+        String[] elements = inputString.split("\\.");
+        for (String element : elements) {
+            if (Integer.parseInt(element) > 255) return false;
+        }
         return true;
     }
 }
